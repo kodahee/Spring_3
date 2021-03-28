f0be3cb70331f7a31495ea7a1d3b5bf395895094
kodahee <daheeko0604@naver.com>
2021-03-29 01:54:03 +0900

    update delete

--- src/main/java/com/lalalala/s3/notice/NoticeController.java
+++ src/main/java/com/lalalala/s3/notice/NoticeController.java
@@ -6,12 +6,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.lalalala.s3.member.MemberDTO;
 
 @Controller
 @RequestMapping(value = "/notice/*")
@@ -39,15 +35,16 @@ public class NoticeController {
 	
 	@RequestMapping(value = "noticeInsert", method = RequestMethod.POST)
 	public String setInsert(NoticeDTO noticeDTO, HttpSession session) throws Exception {
-//		MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
-//		noticeDTO.setName(memberDTO.getName()); 
 		int result = noticeService.setInsert(noticeDTO);
+		session.setAttribute("notice", noticeDTO);
 		return "redirect:noticeList";
 	}
 	
 	@RequestMapping(value = "noticeDelete")
 	public String noticeDelete(NoticeDTO noticeDTO, HttpSession session) throws Exception {
+		noticeDTO = (NoticeDTO)session.getAttribute("notice");
 		int result = noticeService.setDelete(noticeDTO);
+		System.out.println("DELETE : " + result);
 		return "redirect:noticeList";
 	}
 	
